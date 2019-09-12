a9405ffa5e9afef892c351f566c99edd1faad394
abdo hassan <abdocoder0@gmail.com>
2019-09-12 21:55:16 +0200

    created card for every monster

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './card-list.css';
+import Card from './card/Card';
 
-const CardList = props => {
-  return <div>{props.children}</div>;
+const CardList = ({ monsters }) => {
+  return (
+    <div className='card-list'>
+      {monsters.map(monster => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
 };
 
 export default CardList;
---
+++ src/components/card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = ({ monster }) => {
+  return (
+    <div>
+      <h1>{monster.name}</h1>
+    </div>
+  );
+};
+
+export default Card;
